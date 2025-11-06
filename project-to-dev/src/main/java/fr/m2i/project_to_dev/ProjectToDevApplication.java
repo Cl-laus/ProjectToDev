075ca1552036a6806240129434c568570fd296ca
Cl-laus <lucasluisetti@gmail.com>
2025-11-06 16:32:00 +0100

    first commit

---
+++ project-to-dev/src/main/java/fr/m2i/project_to_dev/ProjectToDevApplication.java
@@ -0,0 +1,13 @@
+package fr.m2i.project_to_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectToDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectToDevApplication.class, args);
+	}
+
+}
---
+++ project-to-dev/src/test/java/fr/m2i/project_to_dev/ProjectToDevApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.m2i.project_to_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectToDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
